50bda2d68ff477d2e12823d5ce85b72e82def59a
NelsonBlanco <89229615+NelsonBlanco@users.noreply.github.com>
2022-02-26 20:52:07 -0600

    Add files via upload

---
+++ piano/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Piano></Piano>
+</template>
+
+<script>
+import Piano from './components/Piano';
+
+export default {
+  name: 'App',
+  components: {
+    Piano
+  }
+}
+</script>
+
+<style>
+
+</style>
